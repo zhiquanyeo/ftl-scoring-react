87230b01c87e9e7d696bfd315536c41c519f4792
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2017-11-09 08:10:27 -0500

    Add ADd Match fields

--- client/src/containers/Admin.jsx
+++ client/src/containers/Admin.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, Panel, PanelGroup } from 'react-bootstrap';
+import ReactDOM from 'react-dom';
+import { Grid, Row, Col, Panel, PanelGroup, Form, FormGroup, FormControl, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
 import CurrentMatchView from '../components/CurrentMatchView'
@@ -8,10 +9,16 @@ import MatchAdminList from '../components/MatchAdminList';
 import MatchUtil from '../utils/matchUtil';
 
 class AdminView extends Component {
+    handleAddMatch() {
+        var matchNameElem = ReactDOM.findDOMNode(this.addMatchName);
+        var redTeamsElem = ReactDOM.findDOMNode(this.addRedTeams);
+        var blueTeamsElem = ReactDOM.findDOMNode(this.addBlueTeams);
+        console.log('Match Name: ', matchNameElem.value);
+    }
+
     render() {
-        console.log('admin view render', this.props);
         var activeMatch = MatchUtil.getActiveMatch(this.props.tournamentInfo);
-        console.log('activeMAtch: ', activeMatch);
+        
         return (
             <Grid>
                 <Row>
@@ -28,6 +35,21 @@ class AdminView extends Component {
                             <Panel header="Teams" bsStyle="warning" eventKey="adminTeams">Teams</Panel>
                             <Panel header="Matches" bsStyle="success" eventKey="adminMatches">
                                 <MatchAdminList />
+                                <Form inline>
+                                    <FormGroup>
+                                        <FormControl ref={(input) => { this.addMatchName = input; }} type="text" placeholder="Match Name"/>
+                                    </FormGroup>
+                                    {' '}
+                                    <FormGroup>
+                                        <FormControl ref={(input) => { this.addRedTeams = input; }} type="text" placeholder="Red Teams"/>
+                                    </FormGroup>
+                                    {' '}
+                                    <FormGroup>
+                                        <FormControl ref={(input) => { this.addBlueTeams = input; }} type="text" placeholder="Blue Teams"/>
+                                    </FormGroup>
+                                    {' '}
+                                    <Button bsStyle="primary" onClick={(e) => this.handleAddMatch() }>Add Match</Button>
+                                </Form>
                             </Panel>
                         </PanelGroup>
                     </Col>
